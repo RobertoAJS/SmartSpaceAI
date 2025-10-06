12f6a0306f57dd8f0d9afc862015d4f1eb939df3
Roberto JS <robertojs_03@hotmail.com>
2025-10-06 00:37:46 -0500

    Borrar lo repetido

--- repositories/IUsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.smartspace.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.smartspace.entities.Usuario;
-
-public interface IUsuarioRepository extends JpaRepository<Usuario, Long> {
-    Usuario findOneByUsername(String username);
-}
-
